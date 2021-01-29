f18e64867c9ad2f21054cb424be77c88e5fbbfe5
Aleksey <alexcorpetf@gmail.com>
2021-01-29 21:08:27 +1000

    Lesson Seven
    Выполнил задание из методички

---
+++ LessonSeven/src/main/java/ru/geekbrains/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains;
+
+import ru.geekbrains.handlers.TestHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        TestHandler.start(TestingClass.class);
+    }
+}
